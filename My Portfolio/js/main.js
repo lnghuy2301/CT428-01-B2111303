4ba0e4bf3249ff48f41721f5dec23ea9bdffe403
lnghuy2301 <lnghuy2301@gmail.com>
2024-02-03 23:24:50 +0700

    TH1

---
+++ My Portfolio/js/main.js
@@ -0,0 +1,15 @@
+// Menu
+const menu = document.querySelector(".navbar__links")
+const menuButton = document.querySelector(".navbar__icons")
+
+menuButton.addEventListener('click', ()=>{
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+    overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+    overlay.classList.toggle("show");
+  });
